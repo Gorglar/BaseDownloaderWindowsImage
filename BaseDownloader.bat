1b52a666007bdb808da1e15e9a55730c3e8d97a4
Gorglar <123326814+Gorglar@users.noreply.github.com>
2025-11-06 12:03:16 +0300

    BaseDownloader.bat

--- BaseDownloader.bat
+++ BaseDownloader.bat
@@ -1,12 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
+chcp 1251 >nul
 title Base Downloader
 
-:: РЎРѕР·РґР°РЅРёРµ РІСЂРµРјРµРЅРЅРѕР№ РїР°РїРєРё
-set "temp_dir=%TEMP%\\downloader"
+:: Создание временной папки
+set "temp_dir=%TEMP%\downloader"
 if not exist "%temp_dir%" mkdir "%temp_dir%"
 
-:: РњР°СЃСЃРёРІ (РЎСЃС‹Р»РєР°|РќР°РёРјРµРЅРѕРІР°РЅРёРµ)
+:: Массив (Ссылка|Наименование)
 set "progs[1]=https://aka.ms/vs/17/release/vc_redist.x64.exe|VC redist 2015-2022"
 set "progs[2]=https://builds.dotnet.microsoft.com/dotnet/Sdk/6.0.428/dotnet-sdk-6.0.428-win-x64.exe|NET Framework 6.0"
 set "progs[3]=https://go.microsoft.com/fwlink/?linkid=2109047&Channel=Stable&language=ru&brand=M100|Microsoft Edge"
@@ -21,20 +22,21 @@ set "progs[11]=https://mirror.yandex.ru/mirrors/ftp.videolan.org/vlc/3.0.20/win6
 set "progs[12]=https://unlimited.dl.sourceforge.net/project/qbittorrent/qbittorrent-win32/qbittorrent-5.1.2/qbittorrent_5.1.2_x64_setup.exe?viasf=1|Qbittorrent"
 set "progs[13]=https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user|Visual Studio Code"
 
+:: Меню
 :menu
 cls
-echo Version 1.1
+echo Версия 2.0
 echo ================================
-echo Preferred
+echo Желательное
 echo 1 - VC redist 2015-2022
 echo 2 - NET Framework 6.0
 echo ================================
-echo Browsers
+echo Браузеры
 echo 3 - Microsoft Edge
 echo 4 - Google Chrome
 echo 5 - Firefox
 echo ================================
-echo Optional
+echo Опциональное
 echo 6 - Steam
 echo 7 - 7-Zip
 echo 8 - Spotify
@@ -44,20 +46,27 @@ echo 11 - VLC Player
 echo 12 - Qbittorrent
 echo 13 - Visual Studio Code
 echo ================================
+echo Твикеры
+echo 14 - Показывать/скрывать все иконки в трее
+echo ================================
 echo 0 - Exit
 echo ================================
 
 set /p choice="Enter number: "
-if "%choice%"=="0" exit
+if "%choice%"=="0" exit /b
 
-:: РџСЂРѕРІРµСЂРєР° РЅР°Р»РёС‡РёСЏ С‡РёСЃР»Р° РІ РјР°СЃСЃРёРІРµ (РїРѕРґР»РёРЅРЅРѕСЃС‚СЊ РІС‹Р±РѕСЂР°)
+:: Проверка наличия числа в массиве (подлинность выбора)
 if not defined progs[%choice%] (
-    echo Invalid choice. Please try again...
+    if "%choice%"=="14" (
+        call :tray_tweaker
+        goto menu
+    )
+    echo Недействительный выбор. Попробуйте ещё раз...
     timeout /t 5 >nul
     goto menu
 )
 
-:: РќР°С‡Р°Р»Рѕ Р±Р»РѕРєР° РІС‹РїРѕР»РЅРµРЅРёСЏ СѓСЃС‚Р°РЅРѕРІРєРё
+:: Начало блока выполнения установки
 call :install_program %choice%
 goto menu
 
@@ -68,19 +77,164 @@ for /f "tokens=1,2 delims=|" %%a in ("!prog_data!") do (
     set "description=%%b"
 )
 
-echo Installing !description!...
-set "file_name=!temp_dir!\\installer_%1.exe"
+echo Установка !description!...
+set "file_name=!temp_dir!\installer_%1.exe"
 
-:: РЎРєР°С‡РёРІР°РЅРёРµ Рё СѓСЃС‚Р°РЅРѕРІРєР°
+:: Скачивание и установка
 powershell -Command "Invoke-WebRequest -Uri '!url!' -OutFile '!file_name!'" && (
-    echo Download is completed. Launching the installation...
+    echo Скачивание завершено. Установка...
     start /wait "" "!file_name!"
     del "!file_name!" 2>nul
-    echo Installing !description! is completed.
+    echo Установка !description! завершена.
 ) || (
-    echo Error during download or installation !description!!
+    echo Ошибка установки... !description!!
     if exist "!file_name!" del "!file_name!" 2>nul
 )
 
 timeout /t 5 >nul
+exit /b
+
+:: Твикер всех иконок в трее
+:tray_tweaker
+cls
+echo ================================
+echo Твикер иконок системного трея
+echo ================================
+
+:: Проверяем существование задачи в планировщике
+echo Проверка состояния автозапуска иконок трея...
+schtasks /query /tn "ShowAllTrayIcons" >nul 2>&1
+
+if !errorlevel! == 0 (
+    echo [ОТКЛЮЧЕНИЕ] Задача найдена - удаляем...
+    goto :DisableMode
+) else (
+    echo [ВКЛЮЧЕНИЕ] Задача не найдена - создаем...
+    goto :EnableMode
+)
+
+:: Процесс включения задачи
+:EnableMode
+echo ВКЛЮЧЕНИЕ показа всех иконок трея с автозапуском...
+
+:: Создаем VBS скрипт для показа иконок
+set "VBSFile=%TEMP%\ShowAllTrayIcons.vbs"
+(
+echo HKCU = ^&H80000001
+echo key = "Control Panel\NotifyIconSettings"
+echo Set reg = GetObject^("winmgmts://./root/default:StdRegProv"^)
+echo If reg.EnumKey^(HKCU, key, names^) = 0 Then
+echo     If Not IsNull^(names^) Then
+echo         For Each name In names
+echo             reg.SetDWORDValue HKCU, key + "\" + name, "IsPromoted", 1
+echo         Next
+echo     End If
+echo End If
+) > "%VBSFile%"
+
+:: Создаем XML для задачи планировщика
+set "XMLFile=%TEMP%\ShowAllTrayIcons.xml"
+(
+echo ^<?xml version="1.0" encoding="UTF-16"?^>
+echo ^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>
+echo   ^<Triggers^>
+echo     ^<LogonTrigger^>
+echo       ^<Enabled^>true^</Enabled^>
+echo     ^</LogonTrigger^>
+echo   ^</Triggers^>
+echo   ^<Principals^>
+echo     ^<Principal id="Author"^>
+echo       ^<UserId^>%USERDOMAIN%\%USERNAME%^</UserId^>
+echo       ^<LogonType^>InteractiveToken^</LogonType^>
+echo       ^<RunLevel^>LeastPrivilege^</RunLevel^>
+echo     ^</Principal^>
+echo   ^</Principals^>
+echo   ^<Settings^>
+echo     ^<MultipleInstancesPolicy^>IgnoreNew^</MultipleInstancesPolicy^>
+echo     ^<DisallowStartIfOnBatteries^>false^</DisallowStartIfOnBatteries^>
+echo     ^<StopIfGoingOnBatteries^>false^</StopIfGoingOnBatteries^>
+echo     ^<AllowHardTerminate^>true^</AllowHardTerminate^>
+echo     ^<StartWhenAvailable^>false^</StartWhenAvailable^>
+echo     ^<RunOnlyIfNetworkAvailable^>false^</RunOnlyIfNetworkAvailable^>
+echo     ^<IdleSettings^>
+echo       ^<StopOnIdleEnd^>true^</StopOnIdleEnd^>
+echo       ^<RestartOnIdle^>false^</RestartOnIdle^>
+echo     ^</IdleSettings^>
+echo     ^<AllowStartOnDemand^>true^</AllowStartOnDemand^>
+echo     ^<Enabled^>true^</Enabled^>
+echo     ^<Hidden^>false^</Hidden^>
+echo     ^<RunOnlyIfIdle^>false^</RunOnlyIfIdle^>
+echo     ^<WakeToRun^>false^</WakeToRun^>
+echo     ^<ExecutionTimeLimit^>PT72H^</ExecutionTimeLimit^>
+echo     ^<Priority^>7^</Priority^>
+echo   ^</Settings^>
+echo   ^<Actions Context="Author"^>
+echo     ^<Exec^>
+echo       ^<Command^>wscript.exe^</Command^>
+echo       ^<Arguments^>"%VBSFile%"^</Arguments^>
+echo     ^</Exec^>
+echo   ^</Actions^>
+echo ^</Task^>
+) > "%XMLFile%"
+
+:: Регистрируем задачу в планировщике
+schtasks /create /tn "ShowAllTrayIcons" /xml "%XMLFile%" /f >nul
+
+:: Немедленно применяем настройки для текущей сессии
+cscript //nologo "%VBSFile%"
+
+echo Все иконки трея ВКЛЮЧЕНЫ
+echo Автозапуск АКТИВИРОВАН - будет работать при каждом входе в систему
+goto :RestartExplorer
+
+:: Процесс удаления задачи
+:DisableMode
+echo ОТКЛЮЧЕНИЕ показа всех иконок трея...
+
+:: Удаляем задачу из планировщика
+schtasks /delete /tn "ShowAllTrayIcons" /f >nul
+
+:: Создаем VBS скрипт для скрытия иконок (возврат к авторежиму)
+set "VBSFile=%TEMP%\HideTrayIcons.vbs"
+(
+echo HKCU = ^&H80000001
+echo key = "Control Panel\NotifyIconSettings"
+echo Set reg = GetObject^("winmgmts://./root/default:StdRegProv"^)
+echo If reg.EnumKey^(HKCU, key, names^) = 0 Then
+echo     If Not IsNull^(names^) Then
+echo         For Each name In names
+echo             reg.DeleteValue HKCU, key + "\" + name, "IsPromoted"
+echo         Next
+echo     End If
+echo End If
+) > "%VBSFile%"
+
+:: Применяем настройки скрытия
+cscript //nologo "%VBSFile%"
+
+echo Все иконки трея ОТКЛЮЧЕНЫ (авторежим)
+echo Автозапуск УДАЛЕН
+
+:RestartExplorer
+echo Перезапуск проводника для применения изменений...
+taskkill /f /im explorer.exe >nul 2>&1
+timeout /t 1 /nobreak >nul
+start explorer.exe
+
+:: Очистка временных файлов
+if exist "%TEMP%\ShowAllTrayIcons.vbs" del "%TEMP%\ShowAllTrayIcons.vbs" >nul 2>&1
+if exist "%TEMP%\HideTrayIcons.vbs" del "%TEMP%\HideTrayIcons.vbs" >nul 2>&1
+if exist "%TEMP%\ShowAllTrayIcons.xml" del "%TEMP%\ShowAllTrayIcons.xml" >nul 2>&1
+
+echo Готово! Текущий статус: 
+schtasks /query /tn "ShowAllTrayIcons" >nul 2>&1
+if !errorlevel! == 0 (
+    echo [ВКЛЮЧЕНО] - Все иконки показываются, автозапуск активен
+) else (
+    echo [ОТКЛЮЧЕНО] - Иконки в авторежиме, автозапуск не активен
+)
+
+echo.
+echo Возврат в главное меню через 10 секунд, можете нажать любую кнопку...
+timeout /t 10 >nul
 exit /b
